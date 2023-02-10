6a15afe633f08fdd53cb7fb029bded80e22114b0
Parag Shah <parag.shah@outlook.com>
2023-02-09 21:17:46 -0800

    Fixed batch file to use script by double clicking from desktop.

--- ai-virtualassistant.bat
+++ ai-virtualassistant.bat
@@ -1,7 +1,1 @@
-d:
-cd d:\projects\openai-virtualassistant\
-.\venv\Scripts\activate.bat
-sleep 3
-python main.py 
-
-pause
+d: && cd d:\projects\openai-virtualassistant\ && .\venv\Scripts\activate.bat && python main.py && pause
